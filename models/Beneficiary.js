e63cb7111c7b1f625da60dd77747fc60b1916565
a1000rhan <1000rhan@gmail.com>
2022-03-07 15:45:03 +0300

    ben

--- models/Beneficiary.js
+++ models/Beneficiary.js
@@ -5,13 +5,7 @@ const mongooseSlugPlugin = require("mongoose-slug-plugin");
 const BeneficiarySchema = new mongoose.Schema(
   {
     fullname: { type: String, required: true },
-    IBAN: {
-      type: String,
-      uppercase: true,
-      maxlength: 10,
-      minlength: 10,
-      required: true,
-    },
+    IBAN: { type: String, uppercase: true, maxlength: 16, minlength: 4 },
     address: String,
   },
 
