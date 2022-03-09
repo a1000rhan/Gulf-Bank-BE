c213c5ded1a7d921f59494b18b28246ef78a7b64
TaherNaqi <eng-naqi@hotmail.com>
2022-03-09 09:59:55 +0300

    timestamp

--- models/Transaction.js
+++ models/Transaction.js
@@ -1,11 +1,14 @@
 const mongoose = require("mongoose");
-const TransactionSchema = new mongoose.Schema({
-  method: {
-    type: String,
+const TransactionSchema = new mongoose.Schema(
+  {
+    method: {
+      type: String,
+    },
+    amount: { type: Number, min: 1 },
+    account: { type: mongoose.Schema.Types.ObjectId, ref: "Account" },
+    //   Beneficiaries: [{ type: mongoose.Schema.Types.ObjectId, ref: "Beneficiary" }],
   },
-  amount: { type: Number, min: 1 },
-  account: { type: mongoose.Schema.Types.ObjectId, ref: "Account" },
-  //   Beneficiaries: [{ type: mongoose.Schema.Types.ObjectId, ref: "Beneficiary" }],
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Transaction", TransactionSchema);
